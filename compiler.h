60c794307ab15694a5417fd537a171f4f674cb8e
david_ferrari <ferraridavid.hub@gmail.com>
2025-09-07 15:14:17 +0200

    Compiling alternations and concatenations.

--- compiler.h
+++ compiler.h
@@ -5,10 +5,16 @@
 #include "machine.h"
 
 
-typedef struct StateList {
+typedef union StateList {
     State *s;
-    struct StateList *next;
+    union StateList *next;
 } StateList;
+
+
+StateList* list(State **out) {
+    return (StateList*) out;
+}
+
 
 
 typedef struct Fragment {
@@ -16,6 +22,104 @@ typedef struct Fragment {
     StateList *out;
 } Fragment;
 
-Machine *compile_pattern(char *s) {
-    return NULL;
+
+Fragment fragment(State *in, StateList* out) {
+    Fragment f = {in, out};
+    return f;
 }
+
+
+void patch(StateList *out, State *in) {
+    StateList *next;
+    for (; out; out = next) {
+        next = out->next;
+        out->s = in;
+    }
+}
+
+
+StateList *append(StateList *l1, StateList *l2) {
+    StateList *cur = l1;
+    while (cur->next) {
+        cur = cur->next;
+    }
+    cur->next = l2;
+
+    return l1;
+}
+
+
+char *pattern;
+
+
+Fragment primary() {
+    State *s = malloc(sizeof(State));
+    if (s == NULL) {
+        perror("primary");
+        exit(1);
+    }
+
+    s->c = *pattern++;
+    s->next = NULL;
+    s->next_2 = NULL;
+
+    return fragment(s, list(&(s->next)));
+}
+
+
+Fragment concat() {
+    Fragment head = primary();
+    if (*pattern && *pattern != '|') {
+        Fragment tail = concat();
+        patch(head.out, tail.in);
+        head.out = tail.out;
+    }
+    return head;
+}
+
+
+Fragment alternation() {
+    Fragment left = concat();
+    if (*pattern == '|') {
+        pattern++;
+        Fragment right = alternation();
+
+        State *s = malloc(sizeof(State));
+        if (!s) {
+            perror("alternation");
+            exit(EXIT_FAILURE);
+        }
+
+        s->c = BRANCH;
+        s->next = left.in;
+        s->next_2 = right.in;
+
+        left.in = s;
+        left.out = append(left.out, right.out);
+    }
+
+    return left;
+}
+
+
+
+Machine compile_pattern(char *s) {
+    pattern = s;
+    Fragment f = alternation();
+    patch(f.out, &MATCH_STATE);
+
+    Machine m;
+    machine_init(&m, 2, f.in);
+
+    return m;
+}
+
+
+
+
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -13,11 +13,9 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    Machine *m = compile_pattern(*++argv);
-    if (m==NULL) {
-        return EXIT_FAILURE;
-    }
-    if (machine_match(m, *++argv)){
+    Machine m = compile_pattern(*++argv);
+
+    if (machine_match(&m, *++argv)){
         printf("%s\n", *argv);
     }
 
@@ -29,7 +27,7 @@ int main(int argc, char *argv[]) {
      * the machine can contain loops (I shall study 
      * some technique for directed graphs)
      */
-    machine_free(m);
+    machine_free(&m);
 
     return EXIT_SUCCESS;
 }
